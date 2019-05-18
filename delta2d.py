2084cee3a038728259fe7a0c1204e83ce210a909
Bill Ola Rasmussen <billra@gmail.com>
2019-05-18 18:10:36 +0200

    documentation: diagram and legend

--- delta2d.py
+++ delta2d.py
@@ -1,6 +1,30 @@
 # delta2d plotter kinematics
 # Bill Ola Rasmussen
-# version 0.1
+# version 0.2
+
+# diagram:
+# 
+#   |                |
+#  ↑|                |
+#  ↓|\               |↑
+#   |  \ A          /|↓
+#   |    \      B /  |
+#   |      \    /    |
+#   |        \/      |
+#   |       /        |
+#   |     /          |
+#   |   (x,y)        |
+#   |                |
+# (0,0)            (w,0)
+#
+# legend:
+#
+# | = rail
+# \ = A arm
+# / = B arm
+# ↑↓ = arm upper tip travels up and down rail
+# (x,y) = resulting position of lower arm tip
+
 
 if __name__ == "__main__":
 	pass
