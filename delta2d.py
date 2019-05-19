f505f478df4bdaf67e7214fd3da1ba4c50e06144
Bill Ola Rasmussen <billra@gmail.com>
2019-05-19 17:26:51 +0200

    forward kinematics in progress with circle intersect code

--- delta2d.py
+++ delta2d.py
@@ -31,9 +31,26 @@
 
 import math
 
-def dx(x1,y1,x2,y2):
-	'distance formula'
-	return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+def circle_intersect(x1,y1,r1,x2,y2,r2):
+	'calculate intersection of circles, return lowest point of intersection'
+	dx = x2 - x1	# distance between circle centers on the x axis
+	dy = y2 - y1	# distance between circle centers on the y axis
+	ds = math.sqrt(dx ** 2 + dy ** 2)	# distance between circle centers
+
+	assert ds < r1 + r2, 'no solution, circles do not overlap'
+	assert ds > abs(r1 - r2), 'no solution, one circle contains the other circle'
+	assert ds != 0 or r1 != r2, 'infinite solutions, circles are coincident'
+
+	a = (r1 ** 2 - r2 ** 2 + ds ** 2) / (2 * ds)
+	h = math.sqrt(r1 ** 2 - a ** 2)
+	xm = x1 + a * dx / ds
+	ym = y1 + a * dy / ds
+	xs1 = xm + h * dy / ds
+	ys1 = ym - h * dx / ds
+	# xs2 = xm - h * dy / ds
+	# ys2 = ym + h * dx / ds	special case: we are only interested in the lowest solution
+	return xs1,ys1
+
 
 class Delta2d:
 	'kinematics for delta2d plotter'
@@ -56,12 +73,15 @@ class Delta2d:
 		b = B_height + y # B arm upper tip at (w,b)
 		ix = B_width * self.B_tip_pct + x # ix == A_width, width of rectangle containing arm A
 		iy = B_height * self.B_tip_pct + y
+		print("debug:",ix,iy)
 		A_height = math.sqrt(self.A_len ** 2 - ix ** 2) # height of rectangle containing arm A, using Pythagorean theorem
 		a = A_height + iy # A arm upper tip at (0,a)
 		return a,b
 
 	def xy(self,a,b):
 		'forward kinematics: given a and b positions, calculated the resulting (x,y) position'
+		ix,iy=circle_intersect(0,a,self.A_len,self.width,b,self.B_nontip_len)
+		print("debug:",ix,iy)
 		return 0,0
 
 def main():
