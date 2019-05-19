dc8add34d613329b120ad7a03289eff27d6b694e
Bill Ola Rasmussen <billra@gmail.com>
2019-05-19 09:08:56 +0200

    delta2d class skeleton and documentation

--- delta2d.py
+++ delta2d.py
@@ -1,6 +1,6 @@
 # delta2d plotter kinematics
 # Bill Ola Rasmussen
-# version 0.3
+# version 0.4
 
 # diagram:
 # 
@@ -27,6 +27,29 @@
 # b = B arm upper tip rides on rail at (w,b)
 # (x,y) = resulting position of lower arm tip
 
+class Delta2d:
+	'kinematics for delta2d plotter'
+	def __init__(self, a_len, b_len, b_tip_len, width):
+		'''Create a Delta2d plotter kinematics object
+		:param float a_len: length of A arm
+		:param float b_len: length of B arm (includes b_tip_len)
+		:param float b_tip_len: length from A,B hinge and lower B tip
+		:param float width: width between rails'''
+		pass
+	def ab(self,x,y):
+		'inverse kinematics: given a (x,y) position, calculated the required a and b positions'
+		pass
+	def xy(self,a,b):
+		'forward kinematics: given a and b positions, calculated the resulting (x,y) position'
+		pass
+
+def main():
+	print('creating delta2d object...')
+	plotter = Delta2d(10,10,2,10)
+	print('done.')
 
 if __name__ == "__main__":
-	pass
+	main()
+
+# notes:
+# - sphinx syntax https://www.sphinx-doc.org for documentation
